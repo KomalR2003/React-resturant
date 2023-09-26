1ae2325b904c3abc86e4632c47a2824aaecab0cc
KomalR2003 <komalribadiya0106@gmail.com>
2023-09-26 12:02:33 +0530

    first commit

---
+++ src/data/Data.js
@@ -0,0 +1,45 @@
+import Dosa from '../Images/dhosa.jpg';
+import Chola from '../Images/chola.jpg';
+import Idli from  '../Images/idli.jpg';
+import MasalaDosa from '../Images/masala-dosa-.jpg';
+import Paneer from '../Images/paneer.jpg';
+import Gujarati from '../Images/gujarati.jpg';
+
+export const MenuList = [
+    {
+        name: 'Dosa',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        image: Dosa,
+        Price: 300
+    },
+    {
+        name: 'Chola',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        image: Chola,
+        Price: 300
+    },
+    {
+        name: 'Idli',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        image: Idli,
+        Price: 300
+    },
+    {
+        name: 'MasalaDosa',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        image: MasalaDosa,
+        Price: 300
+    },
+    {
+        name: 'Paneer',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        image: Paneer,
+        Price: 300
+    },
+    {
+        name: 'Gujarati',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        image: Gujarati,
+        Price: 300
+    }
+]
---
+++ src/pages/Contact.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import EmailIcon from '@mui/icons-material/Email';
+import CallIcon from '@mui/icons-material/Call';
+
+const Contact = () => {
+  return (
+    <Layout>
+      <Box sx={{
+        my: 5, ml: 10, "& h4": {
+          fontWeight: 'bold',
+          mb: 2,
+        },
+      }}>
+        <Typography variant='h4'>
+          Contact My Resturant
+        </Typography>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. printing and typesetting industry.
+          Ipsum is simply dummy text of the printing and typesetting industry. printing and typesetting industry.
+          printing and typesetting industry. printing and typesetting industry.
+        </p>
+      </Box>
+      <Box sx={{
+        m: 3, width: '600px', ml: 10,
+        "@media (max-width:600px)":{
+            width:'300px',
+            
+        },
+      }}  >
+        <TableContainer component={Paper}>
+          <Table aria-aria-label='contact table'>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ bgcolor: 'black', color: 'white' }} align='center'>Contact Details</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <SupportAgentIcon sx={{ color: 'red', pt: 1 }} /> 1800-00-0000 (tollfree)
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <EmailIcon sx={{ color: 'skyblue', pt: 1 }} /> komal@gmail.com
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <CallIcon sx={{ color: 'green', pt: 1 }} /> 1234567898
+                </TableCell>
+              </TableRow>
+
+            </TableBody>
+          </Table>
+
+        </TableContainer>
+      </Box>
+    </Layout>
+  )
+}
+
+export default Contact;
